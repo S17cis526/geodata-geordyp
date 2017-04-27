3fe36636e0005d5fe025eca96ef088095b84895b
geordyp <geowilliams6@gmail.com>
2017-04-27 14:21:02 -0500

    Complete in-class work

---
+++ public/script.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', function() {
+  if("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      d3.select('body')
+        .insert('div', ':first-child')
+        .attr("class", "location-bar")
+        .text("You are at: " + position.coords.latitude + ", " + position.coords.longitude);
+    })
+  }
+
+  d3.json('/locations.json', function(err, locations) {
+    if (err) return console.log(err);
+    var table = d3.select('body').append('table');
+    table.append('thead')
+      .append('tr')
+      .selectAll('th')
+      .data(['address', 'latitude', 'longitude'])
+        .enter()
+          .append('th')
+          .text(function(d){return d;});
+
+    table.append('tbody')
+      .append('tr')
+      .selectAll('th')
+        .data(locations)
+        .enter()
+          .append('tr')
+          .each(function(d){
+            d3.select(this).append('td').text(d.address);
+            d3.select(this).append('td').text(d.latitude);
+            d3.select(this).append('td').text(d.longitude);
+          })
+  })
+});
